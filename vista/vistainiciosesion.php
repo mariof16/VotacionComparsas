61f9a72cf584623714d64a239e2a1d2c245a2a54
Mario Fuentes <mariofuentesf16.gmail.com>
2024-02-20 10:09:16 +0100

    estilos

---
+++ vista/vistainiciosesion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administración</title>
+    <link rel="stylesheet" href="../estilos/admin-style.css">
+</head>
+<body>
+    <h1>Votación de los Carnavales</h1>
+    <form>
+</form>
+</body>
+</html>
